4828a8aba527fd537635f049fbc790c554c50c89
codehimanshu <himanshuagrawal1998@gmail.com>
2016-12-18 15:47:05 +0530

    update deveopers team

---
+++ temp/dev-team.php
@@ -0,0 +1,205 @@
+<?php
+/*
+Template Name:Developers
+*/
+?>
+<!--team start here-->
+<div class="team-mem" id="team">
+		<div class="container">
+			   <div class="team-main">
+					<div class="about-head">
+			          <h1>OUR TEAM</h1>
+			          <span> </span>
+			        </div>
+						<ul class="ch-grid">
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/seema_mam.jpg);">
+							<div class="ch-info">
+								<h3>Seema Shukla</h3>
+								<p>Mentor</p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/supr.jpg);">
+							<div class="ch-info">
+								<h3>Supratim</h3>
+								<p>Co-Founder</p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/ram.jpg);">
+							<div class="ch-info">
+								<h3>Ram</h3>
+								<p>Co-Founder</p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/tj.jpg);">
+							<div class="ch-info">
+								<h3>Taranjeet</h3>
+								<p> </p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/saurabh.jpg);">
+							<div class="ch-info">
+								<h3>Saurabh</h3>
+								<p></p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/tushar.png);">
+							<div class="ch-info">
+								<h3>Tushar</h3>
+								<p></p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/desi.jpg);">
+							<div class="ch-info">
+								<h3>Deshraj</h3>
+								<p></p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/pankhuri.jpg);">
+							<div class="ch-info">
+								<h3>Pankhuri</h3>
+								<p> </p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/vipul.jpg);">
+							<div class="ch-info">
+								<h3>Vipul</h3>
+								<p></p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/shivbaran.jpg);">
+							<div class="ch-info">
+								<h3>Shiv Baran</h3>
+								<p></p><a href="http://github.com">Github</a>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/aakash.jpg);">
+							<div class="ch-info">
+								<h3>Akash</h3>
+								<p></p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/shashank-e1474506810301.jpg)">
+							<div class="ch-info">
+								<h3>Shashank</h3>
+								<p> </p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/karan-e1474506933262.jpg)">
+							<div class="ch-info">
+								<h3>Karan</h3>
+								<p> </p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/suyash-e1474506858757.jpg)">
+							<div class="ch-info">
+								<h3>Suyash</h3>
+								<p></p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/deepti.jpg);">
+							<div class="ch-info">
+								<h3>Deepti</h3>
+								<p> </p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/ayush.jpg);">
+							<div class="ch-info">
+								<h3>Ayush</h3>
+								<p></p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/abhisingh.jpg);">
+							<div class="ch-info">
+								<h3>Abhishek</h3>
+								<p></p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/IMG-20160903-WA0002-e1472922139363.jpg);">
+							<div class="ch-info">
+								<h3>Abhishek</h3>
+								<p></p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/himanshu.jpg);">
+							<div class="ch-info">
+								<h3>Himanshu</h3>
+								<p></p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/nooreen.jpg);">
+							<div class="ch-info">
+								<h3>Nooreen</h3>
+								<p></p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/sarthak.jpg);">
+							<div class="ch-info">
+								<h3>Sarthak</h3>
+								<p></p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/vibhav.jpg);">
+							<div class="ch-info">
+								<h3>Vibhav</h3>
+								<p></p>
+							</div>
+						</div></a>
+					</li>
+					<li>
+						<a href="#"><div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/divya-e1472921700620.jpg);">
+							<div class="ch-info">
+								<h3>Divya</h3>
+								<p></p>
+							</div>
+						</div></a>
+					</li>
+				</ul>
+					<div class="clearfix"> </div>
+			</div>
+	  </div>
+</div>
+
+<!--team end  here-->
---
+++ temp/dev-teamnew.php
@@ -0,0 +1,244 @@
+<?php
+/*
+Template Name:Developers
+*/
+?>
+<!--team start here-->
+<div class="team-mem" id="team">
+		<div class="container">
+			   <div class="team-main">
+					<div class="about-head">
+			          <h1>OUR TEAM</h1>
+			          <span> </span>
+			        </div>
+			        	<h3><center><u>Past Members</u></center></h3>
+						<ul class="ch-grid">
+					<!-- Old Members -->
+					<li>
+						<div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/supr.jpg);">
+							<div class="ch-info">
+								<h3>Supratim</h3>
+								<p>Co-Founder</p>
+<!-- 								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+ -->							</div>
+						</div></a>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/ram.jpg);">
+							<div class="ch-info">
+								<h3>Ram</h3>
+								<p>Co-Founder</p>
+<!-- 								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+ -->							</div>
+						</div></a>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/tj.jpg);">
+							<div class="ch-info">
+								<h3>Taranjeet</h3>
+								<p> </p>
+<!-- 								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+ -->							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/saurabh.jpg);">
+							<div class="ch-info">
+								<h3>Saurabh</h3>
+								<p></p>
+<!-- 								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+ -->							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/tushar.png);">
+							<div class="ch-info">
+								<h3>Tushar</h3>
+								<p></p>
+<!-- 								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+ -->							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/desi.jpg);">
+							<div class="ch-info">
+								<h3>Deshraj</h3>
+								<p></p>
+<!-- 								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+ -->							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/pankhuri.jpg);">
+							<div class="ch-info">
+								<h3>Pankhuri</h3>
+								<p> </p>
+<!-- 								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+ -->							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/vipul.jpg);">
+							<div class="ch-info">
+								<h3>Vipul</h3>
+								<p></p>
+<!-- 								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+ -->							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/shivbaran.jpg);">
+							<div class="ch-info">
+								<h3>Shiv Baran</h3>
+								<p></p><a href="http://github.com/spyshiv" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="https://www.facebook.com/shiv.baran.singh" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/shivbaran1" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/aakash.jpg);">
+							<div class="ch-info">
+								<h3>Akash</h3>
+								<p></p>
+							</div>
+						</div>
+					</li>
+						</ul>
+					<!-- New Members -->
+						<br>
+						<h3><center><u>Current Members</u></center></h3>
+						<ul class="ch-grid">
+					<li>
+						<div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/shashank-e1474506810301.jpg)">
+							<div class="ch-info">
+								<h3>Shashank</h3>
+								<p> </p>
+<!-- 								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+ -->							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/karan-e1474506933262.jpg)">
+							<div class="ch-info">
+								<h3>Karan</h3>
+								<p> </p>
+<!-- 								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+ -->							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/suyash-e1474506858757.jpg)">
+							<div class="ch-info">
+								<h3>Suyash</h3>
+								<p></p>
+<!-- 								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+ -->							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/deepti.jpg);">
+							<div class="ch-info">
+								<h3>Deepti</h3>
+								<p> </p>
+<!-- 								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+ -->							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/ayush.jpg);">
+							<div class="ch-info">
+								<h3>Ayush</h3>
+								<p></p>
+<!-- 								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+ -->							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/abhisingh.jpg);">
+							<div class="ch-info">
+								<h3>Abhishek Singh</h3>
+								<p></p>
+								<a href="https://github.com/singhsterabhi" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="https://www.fb.com/singhsterabhi" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/singhsterabhi" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/IMG-20160903-WA0002-e1472922139363.jpg);">
+							<div class="ch-info">
+								<h3>Abhishek</h3>
+								<p></p>
+<!-- 								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+ -->							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/divya-e1472921700620.jpg);">
+							<div class="ch-info">
+								<h3>Divya</h3>
+								<p></p>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/himanshu.jpg);">
+							<div class="ch-info">
+								<h3>Himanshu</h3>
+								<p></p>
+								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/nooreen.jpg);">
+							<div class="ch-info">
+								<h3>Nooreen</h3>
+								<p></p>
+								<a href="http://github.com/nooreenharoon" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="https://www.facebook.com/nooreenharoon" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/nooreen-65598312a" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/sarthak.jpg);">
+							<div class="ch-info">
+								<h3>Sarthak</h3>
+								<p></p>
+								<a href="https://github.com/sarthak-97" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="https://www.facebook.com/sarthakgupta97" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><!-- <a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a> -->
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/09/vibhav.jpg);">
+							<div class="ch-info">
+								<h3>Vibhav</h3>
+								<p></p>
+<!-- 								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+ -->							</div>
+						</div>
+					</li>
+				</ul>
+			        	<h3><center><u>Mentors</u></center></h3>
+			        	<ul class="ch-grid">
+					<li>
+						<div class="ch-item" style="background-image: url(<?php print(get_template_directory_uri()); ?>/gdg_assets/images/team/seema_mam.jpg);">
+							<div class="ch-info">
+								<h3>Seema Shukla</h3>
+								<p>Mentor</p>
+<!-- 								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+ -->							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item" style="background-image: url(http://jssgdg.16mb.com/wp-content/uploads/2016/12/anurag-e1481284883767.jpg);">
+							<div class="ch-info">
+								<h3>Anurag Walia</h3>
+								<p>Mentor</p>
+<!-- 								<a href="http://github.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/Octocat-e1480762105480.png"></a><a href="http://facebook.com/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/logo-facebook-e1480762311981.png"></a><a href="https://in.linkedin.com/in/codehimanshu" target="_blank"><img src="http://jssgdg.16mb.com/wp-content/uploads/2016/12/in-e1481286357140.png"></a>
+ -->							</div>
+						</div></a>
+					</li>
+			        	</ul>
+					<div class="clearfix"> </div>
+			</div>
+	  </div>
+</div>
+
+<!--team end  here-->
